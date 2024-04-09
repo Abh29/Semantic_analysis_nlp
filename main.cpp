9db110bcba522940528ae4ea6a0efac839dc29b1
abh29 <boutifourabdelhak@gmail.com>
2024-04-09 07:50:47 +0300

    adding more logic, still needs embeddings

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <ATen/ops/tensor.h>
 #include <algorithm>
 #include <c10/core/DeviceType.h>
+#include <c10/core/ScalarType.h>
 #include <cstddef>
 #include <cstdint>
 #include <exception>
@@ -12,39 +13,43 @@
 #include <memory>
 #include <nlohmann/json.hpp>
 #include <ostream>
+#include <random>
 #include <string>
 #include <strings.h>
 #include <torch/csrc/autograd/generated/variable_factories.h>
 #include <torch/csrc/jit/frontend/lexer.h>
-#include <torch/cuda.h>
 #include <torch/nn/modules/linear.h>
 #include <torch/torch.h>
+#include <torch/types.h>
 #include <unordered_set>
 #include <utility>
 
 using json = nlohmann::json;
 constexpr int threadCount = 11;
-constexpr int64_t tokenCount = 512;
+constexpr int64_t tokenCount = 256;
 constexpr int64_t batch_size = 64;
+constexpr int epochs = 3;
 FullTokenizer *pTokenizers[threadCount] = {};
 std::string stop_words;
 std::unordered_set<size_t> stop_ids;
-auto options = torch::TensorOptions().dtype(torch::kInt64);
+auto options = torch::TensorOptions().dtype(torch::kFloat);
 
 struct Tokenized {
   std::vector<std::wstring> tokens;
   std::vector<int64_t> ids;
   bool set = false;
-  int label = 0;
+  int64_t label = 0;
 };
 
 using batch_ids = std::vector<std::vector<int64_t>>;
 
-std::vector<Tokenized> train_dataset(25001);
-std::vector<Tokenized> test_dataset(25001);
+std::vector<Tokenized> train_dataset;
+std::vector<Tokenized> test_dataset;
 
 std::vector<torch::Tensor> train_tensors;
+std::vector<torch::Tensor> train_labels_tensors;
 std::vector<torch::Tensor> test_tensors;
+std::vector<torch::Tensor> test_labels_tensors;
 
 void normalizeIds(std::vector<int64_t> &ids) {
   // remove stopwords,
@@ -63,7 +68,7 @@ std::string &toLowerCase(std::string &str) {
   return str;
 }
 
-void work(std::string &line, int index, std::vector<Tokenized> &dataset) {
+void work(std::string line, int index, std::vector<Tokenized> *dataset) {
   // read the text tokenize, normalize and save the result
   json data;
   data = json::parse(line);
@@ -72,10 +77,10 @@ void work(std::string &line, int index, std::vector<Tokenized> &dataset) {
   auto tokens = pTokenizers[index % threadCount]->tokenize(text);
   auto ids = pTokenizers[index % threadCount]->convertTokensToIds(tokens);
   normalizeIds(ids);
-  dataset[index].tokens = std::move(tokens);
-  dataset[index].ids = std::move(ids);
-  dataset[index].set = true;
-  dataset[index].label = data["label"];
+  (*dataset)[index].tokens = std::move(tokens);
+  (*dataset)[index].ids = std::move(ids);
+  (*dataset)[index].set = true;
+  (*dataset)[index].label = data["label"];
 }
 
 void readStopWords(const std::string &path) {
@@ -88,10 +93,22 @@ void readStopWords(const std::string &path) {
   auto tokens = tokenizer->tokenize(stop_words);
   auto ids = tokenizer->convertTokensToIds(tokens);
   stop_ids.insert(ids.begin(), ids.end());
+  std::cout << "end read stop words" << std::endl;
+}
+
+void initialize_vector(const std::string &path, std::vector<Tokenized> &v) {
+  int i = 0;
+  std::string line;
+  std::ifstream f(path);
+  while (std::getline(f, line))
+    ++i;
+  f.close();
+  v.resize(i);
 }
 
 void prepareDataSet(const std::string &path, ft::ThreadPool::pointer &tp,
-                    std::vector<Tokenized> &dataset) {
+                    std::vector<Tokenized> *dataset) {
+
   std::ifstream stream(path);
   std::string line;
   json data;
@@ -104,63 +121,125 @@ void prepareDataSet(const std::string &path, ft::ThreadPool::pointer &tp,
   stream.close();
 }
 
-void createTensors(std::vector<torch::Tensor> &tensors,
+void createTensors(std::vector<torch::Tensor> &data_tensors,
+                   std::vector<torch::Tensor> &label_tensors,
                    std::vector<Tokenized> &dataset) {
 
+  std::cout << "creating tensors from dataset size: " << dataset.size()
+            << std::endl;
   std::vector<int64_t> v(tokenCount, 0);
-  auto options = torch::TensorOptions().dtype(torch::kInt64);
   int i = 0;
   while (i < dataset.size()) {
-    if (!dataset[i].set && ++i)
+    if (!dataset[i].set) {
+      std::cout << "continue i: " << i << "\n";
+      ++i;
       continue;
+    }
     torch::Tensor batch_tensor =
         torch::empty({batch_size, tokenCount}, options);
+    torch::Tensor batch_labels = torch::empty({batch_size, 1}, options);
     int j = 0;
     while (j < batch_size) {
       if (i < dataset.size() && dataset[i].set) {
-        batch_tensor[j] = torch::tensor(dataset[i++].ids, options);
+        batch_tensor[j] = torch::tensor(dataset[i].ids, options);
+        batch_labels[j] = torch::tensor(dataset[i].label, options);
       } else {
         batch_tensor[j] = torch::tensor(v, options);
+        batch_labels[j] = torch::tensor(0.0, options);
       }
       ++j;
+      ++i;
     }
-    tensors.push_back(batch_tensor);
+    data_tensors.push_back(batch_tensor);
+    label_tensors.push_back(batch_labels);
   }
-}
-
-void checkData(std::vector<Tokenized> &dataset) {
-  for (auto &s : dataset)
-    if (s.set && s.ids.size() != tokenCount)
-      std::cout << "error size != 512" << std::endl;
+  std::cout << "end create tensors" << std::endl;
 }
 
 struct StraitForwardModel : public torch::nn::Module {
   using pointer = std::shared_ptr<StraitForwardModel>;
 
-  StraitForwardModel(int64_t input_size, int64_t hidden_size,
-                     int64_t output_size) {
-    // Define the first fully connected layer
-    _fc1 = register_module("fc1", torch::nn::Linear(input_size, hidden_size));
-    // Define the second fully connected layer
-    _fc2 = register_module("fc2", torch::nn::Linear(hidden_size, output_size));
+  StraitForwardModel(int64_t input_size, int64_t hidden_size1,
+                     int64_t hidden_size2, int64_t output_size) {
+    // First fully connected layer
+    _fc1 = register_module("fc1", torch::nn::Linear(input_size, hidden_size1));
+    // Second fully connected layer
+    _fc2 =
+        register_module("fc2", torch::nn::Linear(hidden_size1, hidden_size2));
+    // Output layer
+    _fc3 = register_module("fc3", torch::nn::Linear(hidden_size2, output_size));
   }
   // Implement the forward pass
   torch::Tensor forward(torch::Tensor x) {
-    // Apply the first layer and a ReLU activation
+    // Apply the first layer with a ReLU activation function
     x = torch::relu(_fc1->forward(x));
-    // Apply the second layer
-    x = _fc2->forward(x);
-    // Apply a sigmoid activation to get the output probability
-    return torch::sigmoid(x);
+    // Apply the second layer with a ReLU activation function
+    x = torch::relu(_fc2->forward(x));
+    // Apply the output layer; no activation here
+    x = _fc3->forward(x);
+    return x;
   }
 
 private:
-  torch::nn::Linear _fc1{nullptr}, _fc2{nullptr};
+  torch::nn::Linear _fc1{nullptr}, _fc2{nullptr}, _fc3{nullptr};
 };
 
+void shuffle_dataset(std::vector<torch::Tensor> &dataset,
+                     std::vector<torch::Tensor> &labels) {
+
+  unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+  auto engine = std::default_random_engine(seed);
+  int rand_pos = 0;
+  int size = std::min(dataset.size(), labels.size());
+
+  for (int i = 0; i < size; ++i) {
+    rand_pos = std::rand() % size;
+    std::swap(dataset[i], dataset[rand_pos]);
+    std::swap(labels[i], labels[rand_pos]);
+  }
+};
+
+void train(StraitForwardModel::pointer &model) {
+
+  std::cout << "begin training " << std::endl;
+  // Define the Binary Cross Entropy loss
+  //  auto criterion = torch::nn::BCELoss();
+  auto criterion = torch::nn::BCEWithLogitsLoss();
+  // Define the optimizer, passing the network's parameters and specifying the
+  // learning rate
+  torch::optim::SGD optimizer(model->parameters(),
+                              torch::optim::SGDOptions(0.0001));
+  // iterate over the batches
+  for (int i = 0; i < train_tensors.size(); ++i) {
+    // Zero the gradients
+    optimizer.zero_grad();
+    // Forward pass: Compute predicted output by passing inputs to the model
+    // train_tensors[i].to(torch::kFloat);
+    auto output = model->forward(train_tensors[i]);
+    // Calculate the loss
+    auto loss = criterion(output, train_labels_tensors[i]);
+    // Backward pass: Compute gradient of the loss with respect to model
+    // parameters
+    loss.backward();
+    // Perform a single optimization step (parameter update)
+    optimizer.step();
+    if (i % 10 == 0) {
+      std::cout << "Loss after " << i << " iterations: " << loss.item<double>()
+                << std::endl;
+    }
+  }
+}
+
+// TODO: this is as good as a random guesser
+// need to add a word embedding layer for the model
+// in order for it to make any sense
 int main(int argc, char **argv) {
   if (argc < 3)
     return 1;
+
+  std::cout << "GPU "
+            << (torch::cuda::is_available() ? "detected" : "not detected")
+            << std::endl;
 
   try {
     for (int i = 0; i < threadCount; ++i) {
@@ -177,8 +256,10 @@ int main(int argc, char **argv) {
   // prepare the datasets for training and testing
   try {
     readStopWords("english_stop_words.txt");
-    prepareDataSet(argv[1], threadPool, train_dataset);
-    prepareDataSet(argv[2], threadPool, test_dataset);
+    initialize_vector(argv[1], train_dataset);
+    prepareDataSet(argv[1], threadPool, &train_dataset);
+    initialize_vector(argv[2], test_dataset);
+    prepareDataSet(argv[2], threadPool, &test_dataset);
   } catch (std::exception &e) {
     std::cout << e.what() << std::endl;
   }
@@ -186,17 +267,35 @@ int main(int argc, char **argv) {
   // joint all threads
   threadPool.reset();
 
-  checkData(train_dataset);
-  checkData(test_dataset);
-
   // create the model
   StraitForwardModel::pointer model =
-      std::make_unique<StraitForwardModel>(512, 64, 1);
+      std::make_unique<StraitForwardModel>(tokenCount, 128, 32, 1);
+
+  // suffle the training data
+  std::shuffle(train_dataset.begin(), train_dataset.end(),
+               std::default_random_engine(31));
 
   // create the tensors
-  createTensors(train_tensors, train_dataset);
-  createTensors(test_tensors, test_dataset);
+  createTensors(train_tensors, train_labels_tensors, train_dataset);
+  createTensors(test_tensors, test_labels_tensors, train_dataset);
 
+  // int k = 0;
+  // for (auto &t : train_tensors) {
+  //   std::cout << t << std::endl;
+  //   ++k;
+  //   if (k > 10)
+  //     break;
+  // }
+  // std::cout << std::endl;
+
+  // train the model
+  for (int i = 0; i < epochs; ++i) {
+    shuffle_dataset(train_tensors, train_labels_tensors);
+    train(model);
+  }
+
+  std::cout << "endl! " << std::endl;
+  // getchar();
   for (int i = 0; i < threadCount; ++i)
     delete pTokenizers[i];
   return 0;
